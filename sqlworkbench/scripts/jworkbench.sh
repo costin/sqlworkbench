85248e730c23341654b4fefe70c0a209ae176d3d
nobody <nobody@6c36a7f0-0884-4472-89c7-80b7fa7b7f28>
2007-03-31 13:26:32 +0000

    This commit was manufactured by cvs2svn to create tag 'build_94'.

--- sqlworkbench/scripts/jworkbench.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-# Start SQL Workbench/J
-scriptpath=`dirname $0`
-java -Xmx256m -jar $scriptpath/Workbench.jar $@
--- sqlworkbench/scripts/publish-dev.cmd
+++
@@ -1,3 +0,0 @@
-rem set java_home=c:\jdk14
-rem set java_home=d:\Programme\jdk15
-call ant publish-dev
